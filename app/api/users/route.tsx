2b0c91249ccc1e2983c7cfa4dc41249370aaa649
denishuronw21 <notbdenish@gmail.com>
2025-09-02 17:33:13 +0530

    Auth Completed

---
+++ app/api/users/route.tsx
@@ -0,0 +1,35 @@
+import { db } from "@/config/db";
+import { usersTable } from "@/config/schema";
+import { currentUser } from "@clerk/nextjs/server";
+import { eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+import { use } from "react";
+
+export async function POST(request: NextRequest) {
+
+    const user = await currentUser();
+
+    try{
+        //Check if user already exists
+        const users = await db.select().from(usersTable)
+        //@ts-ignore
+        .where(eq(usersTable.email, user?.primaryEmailAddress?.emailAddress))
+
+        //If not create a new user
+        if(users?.length == 0){
+            const result = await db.insert(usersTable).values({
+                //@ts-ignore
+                name: user?.fullName,
+                email: user?.primaryEmailAddress?.emailAddress,
+                credits: 10 
+                //@ts-ignore
+        }).returning({ usersTable })
+        return NextResponse.json(result[0]?.usersTable)
+    }
+     return NextResponse.json(users[0])  
+    
+}
+catch(e){
+        return NextResponse.json(e)
+    }
+}
---
+++ context/UserDetailContext.tsx
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+import { UsersDetail } from "@/app/provider" 
+
+export const UserDetailContext = createContext<any>(undefined);
